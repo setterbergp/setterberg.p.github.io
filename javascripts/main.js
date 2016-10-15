ceca32d65425c65083731544f23ec70d4f86a9ad
setterbergp <setterberg.p@gmail.com>
2016-10-15 22:05:16 +0200

    Added some changes and a node.js server file

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,13 @@
-console.log('This would be the main JS file.');
+var reader = new XMLHttpRequest() || new ActiveXObject('MSXML2.XMLHTTP');
+function loadFile(file) {
+    reader.open('get', file, true);
+    reader.onreadystatechange = displayContents;
+    reader.send(null);
+}
+
+function displayContents() {
+    if(reader.readyState==4) {
+        var el = document.getElementById('main');
+        el.innerHTML = reader.responseText;
+    }
+}
